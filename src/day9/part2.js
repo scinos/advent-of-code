746608b1b66a7a024fbc1fd1844c1229687e070e
Sergio Cinos <scinos@atlassian.com>
2015-12-11 08:24:29 +0100

    Day 9

---
+++ src/day9/part2.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const re = /^(.*?) to (.*?) = ([0-9]+)$/
+
+module.exports = input => {
+    const nodes = {};
+    const lines = input.split('\n');
+    let results = [];
+    let bestDistance = 0;
+    let bestRoute = null;
+
+    function extractNodes(line) {
+        let [, city1, city2, distance] = re.exec(line);
+        distance = Number(distance);
+
+        if (!nodes[city1]) nodes[city1] = {cities:{}}
+        if (!nodes[city2]) nodes[city2] = {cities:{}}
+
+        nodes[city1].name = city1;
+        nodes[city1].cities[city2] = distance;
+
+        nodes[city2].name = city2;
+        nodes[city2].cities[city1] = distance;
+    }
+
+    function f(current, rest) {
+        if (rest.length <= 1) {
+            let permutation = current.concat(rest);
+            let distance = 0;
+            for (let i = 0; i < permutation.length-1; i++) {
+                let p = permutation[i];
+                let pNext = permutation[i+1];
+                distance += nodes[p].cities[pNext];
+            };
+            if (distance > bestDistance) {
+                bestDistance = distance;
+                bestRoute = permutation;
+            }
+        } else {
+            rest.forEach((node, idx) => {
+                f(current.concat(node), rest.slice(0, idx).concat(rest.slice(idx+1)));
+            });
+        }
+    };
+
+    lines.forEach(extractNodes);
+    f([], Object.keys(nodes));
+
+    return bestDistance;
+}
---
+++ test/day9/test.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const part1 = require('../../src/day9/part1.js');
+const part2 = require('../../src/day9/part2.js');
+const expect = require('chai').expect;
+const fs = require('fs');
+
+function assertPart(fn, input, output) {
+    let result = fn(input);
+    expect(result).to.equal(output);
+}
+function assertFilePart(fn, file, output) {
+    assertPart(fn, fs.readFileSync(__dirname + '/' + file, {encoding: 'utf8'}), output);
+}
+
+describe('Day 9', function () {
+    describe('Part 1', function() {
+        const assert = assertPart.bind({}, part1);
+        const assertFile = assertFilePart.bind({}, part1);
+
+        it('Example', function() {
+            assert([
+                "London to Dublin = 464",
+                "London to Belfast = 518",
+                "Dublin to Belfast = 141"
+            ].join("\n"), 605);
+        });
+
+        it('input', function() {
+            assertFile('input.txt', 251);
+        });
+    });
+
+    describe('Part 2', function() {
+        const assert = assertPart.bind({}, part2);
+        const assertFile = assertFilePart.bind({}, part2);
+
+        it('Example', function() {
+            assert([
+                "London to Dublin = 464",
+                "London to Belfast = 518",
+                "Dublin to Belfast = 141"
+            ].join("\n"), 982);
+        });
+
+        it('input', function() {
+            assertFile('input.txt', 898);
+        });
+    });
+
+});
